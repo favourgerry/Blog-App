76159d01b09fdfdf7e9391c9194c1b678dd4ddf3
favourgerry <favourgeremaine@gmail.com>
2025-11-05 16:36:22 +0000

    Create urls.py

---
+++ myblog/urls.py
@@ -0,0 +1,10 @@
+"""
+myproject URL Configuration
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('main.urls')),  # include app urls
+]
